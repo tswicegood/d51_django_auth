33461df05dbd6e5adec981b20c88b7c057a41194
Travis Swicegood <development@domain51.com>
2009-08-05 11:35:55 -0500

    check basic not-authenticated

--- d51_django_auth/tests.py
+++ d51_django_auth/tests.py
@@ -1,9 +1,25 @@
 from django.test import TestCase
-
+from django.http import HttpRequest
 from d51_django_auth.backends import FacebookConnectBackend
+from facebook import Facebook
+import mox
 
 class TestOfFacebookConnectBackend(TestCase):
     def test_returns_none_if_request_not_passed_in(self):
         auth = FacebookConnectBackend()
         self.assertEqual(None, auth.authenticate())
+
+    def test_returns_none_if_check_session_fails(self):
+        req = mox.MockObject(HttpRequest)
+        facebook = mox.MockObject(Facebook)
+        facebook.check_session(req).AndReturn(False)
+        req.facebook = facebook
+        mox.Replay(facebook)
+        mox.Replay(req)
+
+        auth = FacebookConnectBackend()
+        self.assertEqual(None, auth.authenticate(request = req))
+
+        mox.Verify(facebook)
+        mox.Verify(req)
 
