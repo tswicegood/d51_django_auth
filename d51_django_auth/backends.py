9ec246b7e79d63d91ebd3b139d48d47fb7168cc2
Nathaniel Haas <nate@natehaas.com>
2009-08-10 16:54:16 -0500

    Fix: New user accounts are now saved on first visit.
    When connecting to the site for the first time, the new user's account was not
    being created and (consequently) could be not saved.
    
    TypeError on facebook.uid using %d, not %s.
    IntegrityError on user.create not passing in a username.

--- d51_django_auth/backends.py
+++ d51_django_auth/backends.py
@@ -1,3 +1,4 @@
+
 from django.contrib.auth.models import User
 from facebook import Facebook
 
@@ -13,18 +14,17 @@ class FacebookConnectBackend(object):
         if not credentials.has_key('request'):
             return None
         request = credentials['request']
-        
+
         if not request.facebook.check_session(request):
             return None
-        
+
         request.user.backend = FACEBOOK_CONNECT_BACKEND_STRING
         try:
-            user = self.user_manager.get(username = "fb$%d" % request.facebook.uid)
+            user = self.user_manager.get(username = "fb$%s" % request.facebook.uid)
         except User.DoesNotExist:
             response = request.facebook.users.getInfo([request.facebook.uid], ['name'])
             [first_name, last_name] = response[0]['name'].split(' ', 1)
-            user = self.user_manager.create()
-            user.username = "fb$%d" % request.facebook.uid
+            user = self.user_manager.create(username = "fb$%s" % request.facebook.uid)
             user.first_name = first_name
             user.last_name = last_name
             user.set_unusable_password()
