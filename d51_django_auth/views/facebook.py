aba46ebcd831edd157a1a33ee83719315f3d6a3a
Travis Swicegood <development@domain51.com>
2009-08-19 13:54:57 -0500

    Remove the ghetto namespaces.
    Moving all of the facebook_* views into views.facebook.* just for sanity sake.
    This does break BC, but since this doesn't even have a release number yet, that
    shouldn't be a problem. ;-)

--- d51_django_auth/views/facebook.py
+++ d51_django_auth/views/facebook.py
@@ -1,30 +1,30 @@
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
-from django.contrib.auth import authenticate, login
+from django.contrib import auth
 from django.contrib.auth.views import logout as django_logout
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponseRedirect, HttpResponse
 
-def facebook_login(request, redirect_field_name=REDIRECT_FIELD_NAME):
+def login(request, redirect_field_name=REDIRECT_FIELD_NAME):
     """ Handles logging in a user from Facebook Connect
 
         @todo redirect on invalid login
     """
     if not hasattr(request, 'facebook'):
         raise ImproperlyConfigured(
-            'd51_django_auth.views.facebook_login requires that the PyFacebook '
+            'd51_django_auth.views.login requires that the PyFacebook '
             'middleware (facebook.djangofb.FacebookMiddleware) be enabled in '
             'order to login.  Please add it to your site\'s MIDDLEWARE_CLASSES'
         )
 
-    user = authenticate(request = request)
+    user = auth.authenticate(request = request)
     if not user is None:
-        login(request, user)
+        auth.login(request, user)
 
     redirect_to = request.REQUEST.get(redirect_field_name, '/')
     return HttpResponseRedirect(redirect_to)
 
-def facebook_xd_receiver(request):
+def xd_receiver(request):
     return HttpResponse(
 """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -34,7 +34,7 @@ def facebook_xd_receiver(request):
 </body>
 </html>""")
 
-def facebook_logout(request, *args, **kwargs):
+def logout(request, *args, **kwargs):
     """ Handles logging a user out of system after logging out of FB Connect
 
         This passes off to django.contrib.auth.views.logout to handle the
