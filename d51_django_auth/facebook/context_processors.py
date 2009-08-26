f6528e48b9217afdf5acf2c8d93d52fd76305bd3
Nathaniel Haas <nate@natehaas.com>
2009-08-26 00:11:40 -0500

    Move FacebookRequest's facebook into its own context processor.

--- d51_django_auth/facebook/context_processors.py
+++ d51_django_auth/facebook/context_processors.py
@@ -4,3 +4,8 @@ def api_key(request):
     return {
         'FACEBOOK_API_KEY': getattr(settings, 'FACEBOOK_API_KEY', None),
     }
+
+def facebook(request):
+    return {
+        'facebook': getattr(request, 'facebook', None),
+    }
