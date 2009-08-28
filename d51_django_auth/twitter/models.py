00cd1a7231390cd52f7ae0f618ed859914d36624
Travis Swicegood <development@domain51.com>
2009-08-28 17:53:04 -0500

    add a get_twitter() method to help get a Twitter object for the given user

--- d51_django_auth/twitter/models.py
+++ d51_django_auth/twitter/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -9,4 +10,12 @@ class TwitterToken(models.Model):
     def get_oauth_token(self):
         from oauth.oauth import OAuthToken
         return OAuthToken(self.key, self.secret)
+
+    def get_twitter(self):
+        from oauthtwitter import OAuthApi
+        return OAuthApi(
+            settings.D51_DJANGO_AUTH['TWITTER']['CONSUMER_KEY'],
+            settings.D51_DJANGO_AUTH['TWITTER']['CONSUMER_SECRET'],
+            access_token = self.get_oauth_token()
+        )
 
